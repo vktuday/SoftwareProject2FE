6f83fcf4c9d5e7f311e5e5d4b76f5d5828e3d649
vktuday <43793457+vktuday@users.noreply.github.com>
2026-03-10 20:09:44 -0400

    changes to be committed:

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "https://softwareproject2.onrender.com";
+  process.env.REACT_APP_API_BASE_URL || "https://softwareproject2.onrender.com/api";
 
 // Get JWT token from localStorage
 export function getToken() {
